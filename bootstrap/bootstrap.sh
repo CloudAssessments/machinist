2e91ea120b03a1243a6562f791643bb826976117
Travis N. Thomsen <travis.n.thomsen@gmail.com>
2017-10-26 10:29:17 -0500

    update berksfile path

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd ~/machinist/bootstrap
-berks vendor cookbooks
+berks vendor cookbooks -b ~/machinist/bootstrap/Berksfile
 
 chef-solo -c ~/machinist/bootstrap/solo.rb -j ~/machinist/bootstrap/aws.json
