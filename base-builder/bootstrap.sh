6a3f6827f3d3efea99e8bbd2582f8191675e8810
Travis N. Thomsen <travis.n.thomsen@gmail.com>
2017-10-26 10:51:15 -0500

    fix base-builder

--- base-builder/bootstrap.sh
+++ base-builder/bootstrap.sh
@@ -3,4 +3,4 @@
 cd ~/machinist/base-builder
 berks vendor cookbooks -b ~/machinist/base-builder/Berksfile
 
-chef-solo -c ~/machinist/bootstrap/solo.rb -j ~/machinist/base-builder/aws.json
+chef-solo -c ~/machinist/base-builder/solo.rb -j ~/machinist/base-builder/aws.json
