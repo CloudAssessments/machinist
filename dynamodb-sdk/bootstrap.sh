3a2284a37de19c4a2f01b6025b60396198ddd281
Travis N. Thomsen <travis.n.thomsen@gmail.com>
2017-11-17 14:48:31 -0600

    fix machinist-cloudconfig missed one

--- dynamodb-sdk/bootstrap.sh
+++ dynamodb-sdk/bootstrap.sh
@@ -3,4 +3,4 @@
 cd ~/machinist/lfcs
 berks vendor cookbooks -b ~/machinist-cloudconfig/dynamodb-sdk/Berksfile
 
-chef-solo -c ~/machinist-cloudconfig/dynamodb-sdk/solo.rb -j ~/machinist/dynamodb-sdk/aws.json
+chef-solo -c ~/machinist-cloudconfig/dynamodb-sdk/solo.rb -j ~/machinist-cloudconfig/dynamodb-sdk/aws.json
