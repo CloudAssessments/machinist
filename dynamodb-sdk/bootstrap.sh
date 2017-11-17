70fe97f56952efde923e2c5b53369b34c4b4bfc8
Travis N. Thomsen <travis.n.thomsen@gmail.com>
2017-11-17 15:09:04 -0600

    I really suck at coding today

--- dynamodb-sdk/bootstrap.sh
+++ dynamodb-sdk/bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd ~/machinist/dynamodb-sdk
+cd ~/machinist-cloudconfig/dynamodb-sdk
 berks vendor cookbooks -b ~/machinist-cloudconfig/dynamodb-sdk/Berksfile
 
 chef-solo -c ~/machinist-cloudconfig/dynamodb-sdk/solo.rb -j ~/machinist-cloudconfig/dynamodb-sdk/aws.json
