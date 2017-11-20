6830daccccdf5dfb3a2c0f786134651651919ccb
Travis N. Thomsen <travis.n.thomsen@gmail.com>
2017-11-20 16:51:49 -0600

    add home

--- dynamodb-sdk/bootstrap.sh
+++ dynamodb-sdk/bootstrap.sh
@@ -4,6 +4,6 @@ wget https://packages.chef.io/files/stable/chefdk/2.0.28/el/7/chefdk-2.0.28-1.el
 rpm -ivh chefdk-2.0.28-1.el7.x86_64.rpm
 
 cd ~/machinist-cloudconfig/dynamodb-sdk
-berks vendor cookbooks -b ~/machinist-cloudconfig/dynamodb-sdk/Berksfile
+HOME=/root berks vendor cookbooks -b ~/machinist-cloudconfig/dynamodb-sdk/Berksfile
 
 chef-solo -c ~/machinist-cloudconfig/dynamodb-sdk/solo.rb -j ~/machinist-cloudconfig/dynamodb-sdk/aws.json
