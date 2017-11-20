b4de01669b419956864a3331f710b0345a84eb80
Travis N. Thomsen <travis.n.thomsen@gmail.com>
2017-11-20 16:10:59 -0600

    test by installing chef again

--- dynamodb-sdk/bootstrap.sh
+++ dynamodb-sdk/bootstrap.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+wget https://packages.chef.io/files/stable/chefdk/2.0.28/el/7/chefdk-2.0.28-1.el7.x86_64.rpm
+rpm -ivh chefdk-2.0.28-1.el7.x86_64.rpm
+
 cd ~/machinist-cloudconfig/dynamodb-sdk
 berks vendor cookbooks -b ~/machinist-cloudconfig/dynamodb-sdk/Berksfile
 
