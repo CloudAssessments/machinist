ffe3abe9fd14111a523d366e5a0049bfa7e17ec3
Travis N. Thomsen <travis.n.thomsen@gmail.com>
2017-11-21 12:44:27 -0600

    add files and copy them to the ec2 instance

--- dynamodb-sdk/bootstrap.sh
+++ dynamodb-sdk/bootstrap.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-wget https://packages.chef.io/files/stable/chefdk/2.0.28/el/7/chefdk-2.0.28-1.el7.x86_64.rpm
-rpm -ivh chefdk-2.0.28-1.el7.x86_64.rpm
-
 cd ~/machinist-cloudconfig/dynamodb-sdk
 HOME=/root berks vendor cookbooks -b ~/machinist-cloudconfig/dynamodb-sdk/Berksfile
 
 chef-solo -c ~/machinist-cloudconfig/dynamodb-sdk/solo.rb -j ~/machinist-cloudconfig/dynamodb-sdk/aws.json
+
+/usr/local/bin/npm install aws-sdk
+
+cp ~/machinist-cloudconfig/dynamodb-sdk/files/* /home/cloud_user/
